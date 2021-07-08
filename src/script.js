529ab24249bef9211f9abf29120fc07fac2a99ee
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-07-08 15:44:43 -0700

    add pointlight2 and helper for pointlight2

--- src/script.js
+++ src/script.js
@@ -3,6 +3,11 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "dat.gui";
 
+// Loading
+const textureLoader = new THREE.TextureLoader();
+
+const normalTexture = textureLoader.load("/textures/NormalMap.png");
+
 // Debug
 const gui = new dat.GUI();
 
@@ -20,7 +25,8 @@ const geometry = new THREE.SphereBufferGeometry(0.5, 64, 64);
 const material = new THREE.MeshStandardMaterial();
 material.metalness = 0.7;
 material.roughness = 0.2;
-material.color = new THREE.Color(0xff0000);
+material.normalMap = normalTexture;
+material.color = new THREE.Color(0x292929);
 
 // Mesh
 const sphere = new THREE.Mesh(geometry, material);
@@ -33,6 +39,22 @@ pointLight.position.x = 2;
 pointLight.position.y = 3;
 pointLight.position.z = 4;
 scene.add(pointLight);
+
+const pointLight2 = new THREE.PointLight(0xff0000, 2);
+// pointLight.position.x = 2;
+// pointLight.position.y = 3;
+// pointLight.position.z = 4;
+pointLight2.position.set(1, 1, 1);
+pointLight2.intensity = 2;
+scene.add(pointLight2);
+
+gui.add(pointLight2.position, "y").min(-3).max(3).step(0.01);
+gui.add(pointLight2.position, "x").min(-6).max(6).step(0.01);
+gui.add(pointLight2.position, "z").min(-3).max(3).step(0.01);
+gui.add(pointLight2, "intensity").min(0).max(10).step(0.01);
+
+const pointLight2Helper = new THREE.PointLightHelper(pointLight2, 0.3);
+scene.add(pointLight2Helper);
 
 /**
  * Sizes
